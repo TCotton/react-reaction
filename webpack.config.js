b3a47027ef9f2a8e8b39dac4338571b36a645d87
Andy Walpole <me@andywalpole.me>
2016-11-13 09:25:50 +0000

    added working webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  // having an array means that there can be multiple entry points
+  entry: [
+    './app/index.jsx'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js',
+    sourceMapFilename: "[file].map",
+  },
+  debug: true,
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      // for loaders, always include the exclude, test and loader property
+      {
+        exclude: /node_modules/,
+        test: /\.jsx?$|\.js?$/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }
+    ]
+  },
+  resolve: {
+    // resolve extensions in the ES6 modules import/export
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './app/'
+  }
+};
