8da9098d3696e52607998af84af6559c32603b18
Andy Walpole <me@andywalpole.me>
2016-11-14 13:18:46 +0000

    final laptop commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 let path = require('path');
-var sassLintPlugin = require('sasslint-webpack-plugin');
+let sassLintPlugin = require('sasslint-webpack-plugin');
+let autoprefixer = require('autoprefixer');
+let postcssBase64 = require('postcss-base64');
 
 let sassParams = [
   'outputStyle=expanded',
@@ -10,12 +12,13 @@ let sassParams = [
 sassParams.push('sourceMap', 'sourceMapContents=true');
 
 module.exports = {
+  context: path.resolve('app'),
   // having an array means that there can be multiple entry points
   entry: [
     './app/index.jsx'
   ],
   output: {
-    path: __dirname,
+    //path: path.resolve('./build'),
     publicPath: '/',
     filename: 'bundle.js',
     sourceMapFilename: "[file].map",
@@ -29,12 +32,14 @@ module.exports = {
       ignoreFiles: [
         './app/scss/_reset.scss'
       ]
-  /*    ignorePlugins: [
-        'sass-lint',
-        'sasslint-webpack-plugin'
-      ]*/
-     /* glob: '**!/!*.s?(a|c)ss'*/
     }),
+  ],
+  postcss: [
+    autoprefixer({browsers: ['last 2 versions']}),
+    postcssBase64({extensions: ['.svg', '.png', '.jpeg', 'jpg']}),
+    require('postcss-mq-keyframes'),
+    require('postcss-focus'),
+    require('postcss-fakeid')
   ],
   module: {
     preLoaders: [
@@ -42,11 +47,6 @@ module.exports = {
         exclude: /node_modules/,
         test: /\.jsx?$|\.js$/,
         loader: 'eslint-loader!jscs-loader'
-      },
-      {
-        exclude: /node_modules/,
-        test: /\.scss$/,
-        loader: 'style-loader!css-loader?sourceMap&modules&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader?' + sassParams.join('&')
       }
     ],
     loaders: [
@@ -58,6 +58,16 @@ module.exports = {
         query: {
           presets: ['react', 'es2015', 'stage-1']
         }
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader?sourceMap&modules&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!sass-loader?' + sassParams.join('&')
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        exclude: /node_modules/,
+        loader: 'url-loader?limit=100000'
       }
     ]
   },
