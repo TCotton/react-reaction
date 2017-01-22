d392c276f81dc555aeb5118b66d5f4c6e31deefd
Andy Walpole <me@andywalpole.me>
2017-01-22 19:41:19 +0000

    working webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,107 @@ const sassLintPlugin = require('sasslint-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const postcssBase64 = require('postcss-base64');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const postCSSMqKeyframes = require('postcss-mq-keyframes');
+const postCSSFocus = require('postcss-focus');
+const postCSSFakeId = require('postcss-fakeid');
 
 const sassParams = [
   'outputStyle=expanded',
   'includePaths[]=' + path.resolve(__dirname, './app/scss')
-  //'excludePaths[]=' + path.resolve(__dirname, './node_modules')
 ];
 
 sassParams.push('sourceMap', 'sourceMapContents=true');
 
+function getPostcssPlugins() {
+  return [
+    autoprefixer({
+      cascade: false,
+      browser: ['last 2 versions']
+    }),
+    postcssBase64({
+      extensions: ['.svg', '.png', '.jpeg', 'jpg']
+    }),
+    postCSSMqKeyframes(),
+    postCSSFocus(),
+    postCSSFakeId()
+  ];
+}
+
 module.exports = {
-  context: path.resolve('app'),
-  // having an array means that there can be multiple entry points
+
   entry: [
     './index.jsx'
   ],
+
   output: {
-    path: path.resolve('./build'),
+    path: path.resolve(__dirname, './build'),
     publicPath: 'build',
     filename: 'bundle.js',
     sourceMapFilename: '[file].map'
   },
-  debug: true,
+
+  module: {
+
+    rules: [
+      {
+        test: /\.jsx?$|\.js$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.jsx?$|\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', ['es2015', { 'modules': false }], 'stage-1']
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader?sourceMap&modules&localIdentName=[name]__[local]___[hash:base64:5]'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: getPostcssPlugins
+            }
+          },
+          {
+            loader: 'sass-loader?' + sassParams.join('&')
+          }
+
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpe?g$|svg)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 100000
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  resolve: {
+    // resolve extensions in the ES6 modules import/export
+    extensions: ['.js', '.jsx']
+  },
+
+  context: path.resolve(__dirname, './app'),
   devtool: 'inline-source-map',
+
   plugins: [
 
     new sassLintPlugin({
@@ -61,49 +139,10 @@ module.exports = {
     })
 
   ],
-  postcss: [
-    autoprefixer({ browsers: ['last 2 versions'] }),
-    postcssBase64({ extensions: ['.svg', '.png', '.jpeg', 'jpg'] }),
-    require('postcss-mq-keyframes'),
-    require('postcss-focus'),
-    require('postcss-fakeid')
-  ],
-  module: {
-    preLoaders: [
-      {
-        exclude: /node_modules/,
-        test: /\.jsx?$|\.js$/,
-        loader: 'eslint-loader'
-      }
-    ],
-    loaders: [
-      // for loaders, always include the exclude, test and loader property
-      {
-        exclude: /node_modules/,
-        test: /\.jsx?$|\.js?$/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
-      },
-      {
-        exclude: /node_modules/,
-        test: /\.scss$/,
-        loader: 'style-loader!css-loader?sourceMap&modules&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader!sass-loader?' + sassParams.join('&')
-      },
-      {
-        test: /\.(png|jpg|jpe?g$|svg)$/,
-        exclude: /node_modules/,
-        loader: 'url-loader?limit=100000'
-      }
-    ]
-  },
-  resolve: {
-    // resolve extensions in the ES6 modules import/export
-    extensions: ['', '.js', '.jsx']
-  },
+
   devServer: {
     historyApiFallback: true,
     contentBase: './app/'
   }
+
 };
